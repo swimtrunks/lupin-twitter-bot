d5fd14e7d05d3b07b46044b225fc30657c0d60b7
Malcolm Peterson <mgpetersn@gmail.com>
2022-09-09 14:57:31 -0400

    added profile and updated quotes

--- src/collect_quotes.py
+++ src/collect_quotes.py
@@ -6,3 +6,6 @@ if __name__ == "__main__":
     quotes= _scraper.scrape_quotes()
     with open("quotes.json", mode = "w") as quotes_file:
         _json.dump(quotes, quotes_file, ensure_ascii =False)
+
+
+print("hello")
